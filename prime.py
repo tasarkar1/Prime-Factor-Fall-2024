a2bbd1837ccffa459ff375caf4622dd254194760
tasarkar1 <timsarkar@gmail.com>
2024-10-15 22:37:55 -0400

    Main Prime Factor Code

--- prime.py
+++ prime.py
@@ -1,3 +1,17 @@
 """
 prime.py -- Write the application code here
 """
+
+def generate_prime_factors(n):
+    if type(n) is not int:
+        raise ValueError
+    primelist = []
+    for num in range (2, n):
+         while (n % num) ==0:
+            primelist.append(num)
+            n//=num
+    if n > 1:
+         primelist.append(n)
+    return primelist
+
+
